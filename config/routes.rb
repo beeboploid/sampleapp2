c8c2a5ca42bd3c5809dea068c00f17e67008b40a
Loide <loide.marwanga@yale.edu>
2014-06-29 00:05:04 -0400

    Finish sign-in-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   resources :microposts
 
@@ -7,14 +8,12 @@ Rails.application.routes.draw do
   #match '/', to: 'static_pages#home', via: 'get'
 
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
-  match '/help', to: 'static_pages#help', via: 'get'
-
-  match '/about', to: 'static_pages#about', via: 'get'
-
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-
-  match '/signup', to: 'users#new', via: 'get'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
